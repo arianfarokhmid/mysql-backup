056942bdacc9525fbd5da40ba65db0b53b992114
RadinPirouz <r.pirouz@azkivam.com>
2026-04-29 11:41:11 +0330

    Added Basic functions

--- Full/full_backup.sh
+++ Full/full_backup.sh
@@ -8,6 +8,17 @@ MYSQL_PORT=3306
 MYSQL_HOST=mysql-dev
 MYSQL_DOCKER_NETWORK=mysql
 
+
+MYSQL_TEST_USERNAME=data_cheker
+MYSQL_TEST_PASSWORD=T39EEyFRCCfmeNeQNXMbXVrK
+MYSQL_TEST_IMAGE=mysql:8.0.43
+MYSQL_TEST_CONTAINER_NAME=mysql-test-backup
+MYSQL_TEST_CONTAINER_UID=999
+MYSQL_TEST_DATA_DIR=/opt/mysql-inc-dev-backup/mysql-test-backup-data
+MYSQL_TEST_NETWORK=mysql-test-backup-net
+MYSQL_TEST_HOST_PORT=3309
+
+
 CONTAINER_IMAGE=percona/percona-xtrabackup:8.0.35
 
 S3_ENDPOINT=https://s3.thr2.sotoon.ir
@@ -58,6 +69,26 @@ init_backup_name() {
     MYSQL_COMPRESSED_FILES_DIR=/opt/mysql-inc-dev-backup/final-backups
 }
 
+
+
+verify_chain() {
+    local prev_dir=$1
+    local curr_dir=$2
+
+    # Extract LSNs using grep and awk
+    local prev_to_lsn=$(grep "to_lsn" "$prev_dir/xtrabackup_checkpoints" | awk '{print $3}')
+    local curr_from_lsn=$(grep "from_lsn" "$curr_dir/xtrabackup_checkpoints" | awk '{print $3}')
+
+    if [[ "$prev_to_lsn" == "$curr_from_lsn" ]]; then
+        log "INFO" "Chain Integrity OK: $prev_dir ($prev_to_lsn) -> $curr_dir ($curr_from_lsn)"
+        return 0
+    else
+        log "ERROR" "CORRUPTION DETECTED: Chain broken between $prev_dir and $curr_dir"
+        log "ERROR" "Expected: $prev_to_lsn, Found: $curr_from_lsn"
+        return 1
+    fi
+}
+
 docker_xtrabackup_exec() {
     local extra_args=$1
     docker run -u 999 --rm --network $MYSQL_DOCKER_NETWORK \
@@ -89,6 +120,130 @@ full_backup() {
 }
 
 
+inc_backup() {
+    for (( i=1; i<=MAX_INC_BACKUP_COUNT; i++ )); do
+        target="$MYSQL_BACKUP_DIR/inc$i"
+        if [[ ! -d "$target" ]]; then
+            inc_file="$i"
+            break
+        fi
+    done
+
+    if [[ -z "$inc_file" ]]; then
+        log "WARN" "All incremental slots (1–$MAX_INC_BACKUP_COUNT) are already used."  
+        merge_inc_to_full
+        return 0;
+    fi
+
+    if [[ "$inc_file" -eq 1 ]]; then
+        base_bk="full"
+    else
+        old_inc_file=$((inc_file - 1))
+        base_bk="inc$old_inc_file"
+    fi
+
+    if docker_xtrabackup_exec "--backup --target-dir=/backup/inc$inc_file --incremental-basedir=/backup/$base_bk"; then
+        log "INFO" "Incremental backup inc$inc_file successfully"
+    else
+        log "ERROR" "Incremental backup inc$inc_file failed." >&2
+        return 1
+    fi
+}
+
+
+checks_inc_backups() {
+    for inc_dir in $( ls -d $MYSQL_BACKUP_DIR/inc* | sort -V);
+    do
+        if [[ -d $inc_dir ]]; then 
+            if [[ -e $inc_dir/xtrabackup_checkpoints  ]]; then
+                if [[ -n $latest_inc_dir ]]; then
+                    verify_chain $latest_inc_dir $inc_dir 
+                fi
+            else
+                log "ERROR" "Dir $inc_dir Have Some Issue"
+                return 1
+            fi
+            latest_inc_dir=$inc_dir
+        fi 
+    done
+}
+
+
+merge_inc_to_full() {
+    local merge_state
+    docker_xtrabackup_exec "--prepare  --apply-log-only --target-dir=/backup/full"
+    for (( i=1; i<=MAX_INC_BACKUP_COUNT; i++ )); do
+        target="$MYSQL_BACKUP_DIR/inc$i"
+        if [[ -d "$target" ]]; then
+            if docker_xtrabackup_exec "--prepare --apply-log-only --target-dir=/backup/full --incremental-dir=/backup/inc$i"; then
+                log "INFO" "Incremental backup inc$i merged with full"
+                merge_state=true
+                mv $target "$MYSQL_BACKUP_DIR/merged_inc$i"
+            else
+                log "WARN" "Merge with full Incremental backup inc$i failed"
+                merge_state=false
+            fi
+        fi
+    done    
+
+    if [[ "$merge_state" == true ]]; then 
+        apply_log
+    else
+        log "ERROR" "Incremental backup Merges Failed"
+        exit 1;
+    fi
+    
+}
+
+check_mysql_state() {
+    local retries=240
+    for i in $(seq 1 "$retries"); do
+        if docker exec -i $MYSQL_TEST_CONTAINER_NAME mysql -u $MYSQL_TEST_USERNAME -p"$MYSQL_TEST_PASSWORD" -D azki_loan -e "select * from ticket order by id desc LIMIT 10;"; then
+            return 0
+        fi
+        sleep 1
+    done
+    return 1 
+}
+
+check_temp_mysql_dir() {
+    if ! [ -z "$(ls -A $MYSQL_TEST_DATA_DIR)" ]; then
+        if rm -rf $MYSQL_TEST_DATA_DIR/*; then 
+            log "DONE" "MySQL Test Dir Cleaned"
+        else
+            log "ERROR" "Failed To Clean MySQL Test Dir Data"
+        fi
+    fi
+}
+
+setup_temp_mysql() {
+    if docker_xtrabackup_exec "--copy-back --target-dir=/backup/full --datadir=/var/lib/mysql_new"; then 
+        if docker network create "${MYSQL_TEST_NETWORK}"; then
+            docker run -d \
+                --user "${MYSQL_TEST_CONTAINER_UID}" \
+                --name "${MYSQL_TEST_CONTAINER_NAME}" \
+                --volume "${MYSQL_TEST_DATA_DIR}:/var/lib/mysql" \
+                --network "${MYSQL_TEST_NETWORK}" \
+                --publish "${MYSQL_TEST_HOST_PORT}:3306" \
+                "${MYSQL_TEST_IMAGE}"
+            if check_mysql_state; then 
+                clean_temp_mysql
+                log "DONE" "Test Data On MySQL Temp Successfully"
+            else
+                clean_temp_mysql
+                log "ERROR" "Can Not Excute Test Data On MySQL Temp"
+                exit 1;
+            fi
+        else
+            log "ERROR" "Failed To Create MySQL Test Network"
+        fi
+    else
+        log "ERROR" "Failed To Copy Backup Data To MySQL Test Container"
+    fi
+}
+
+
+
 
 level1_tables() {
     init_backup_name "high-level-1"
@@ -118,17 +273,17 @@ clean_temp_mysql() {
        log "ERROR" "Clean Old Backups Failed"
    fi
 
-#    if clean_files_s3; then
-#        log "DONE" "Clean Old S3 Backups Success"
-#    else
-#        log "ERROR" "Clean Old S3 Backups Failed"
-#    fi
+   if clean_files_s3; then
+       log "DONE" "Clean Old S3 Backups Success"
+   else
+       log "ERROR" "Clean Old S3 Backups Failed"
+   fi
 
-#    if upload_files_s3; then
-#        log "DONE" "Upload Backup To S3 Success"
-#    else
-#        log "ERROR" "Failed To Upload Backup To S3"
-#    fi
+   if upload_files_s3; then
+       log "DONE" "Upload Backup To S3 Success"
+   else
+       log "ERROR" "Failed To Upload Backup To S3"
+   fi
 
 }
 
@@ -170,6 +325,7 @@ clean_files_s3() {
 
 
 main() {
+    init_backup_name
     if [[ ! -d "$MYSQL_BACKUP_DIR/full" ]]; then
         full_backup
         return
@@ -183,5 +339,5 @@ main() {
 }
 
 # main
-init_backup_name
-level1_tables
+#init_backup_name
+#level1_tables

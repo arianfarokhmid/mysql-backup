a09fe0bf6c5ed269ba94c2951571e04f3ad7b415
awwrian <150133638+arianfarokhmid@users.noreply.github.com>
2026-05-05 17:22:46 +0330

    Add config.sh for configuration variables

---
+++ lib/config.sh
@@ -0,0 +1,11 @@
+# Configuration variables
+
+# MySQL credentials
+export MYSQL_USER="your_mysql_user"
+export MYSQL_PASSWORD="your_mysql_password"
+export MYSQL_DATABASE="your_database_name"
+
+# S3 credentials
+export AWS_ACCESS_KEY_ID="your_aws_access_key"
+export AWS_SECRET_ACCESS_KEY="your_aws_secret_key"
+export S3_BUCKET="your_s3_bucket"
